e6a711ec8a03b8c12fcad6806602f9d6dc5a3253
SyrillDex <syrilldexl@gmail.com>
2024-10-23 21:45:29 +0800

    Add to github

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace CsharpPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = "pizza";
+
+            switch (day)
+            {
+                case "Monday":
+                    Console.WriteLine("It is " + day);
+                    break;
+                case "Tuesday":
+                    Console.WriteLine("It is " + day);
+                    break;
+                case "Wednesday":
+                    Console.WriteLine("It is " + day);
+                    break;
+                case "Thursday":
+                    Console.WriteLine("It is " + day);
+                    break;
+                case "Friday":
+                    Console.WriteLine("It is " + day);
+                    break;
+                case "Saturday":
+                    Console.WriteLine("It is " + day);
+                    break;
+                case "Sunday":
+                    Console.WriteLine("It is " + day);
+                    break;
+                default:
+                    Console.WriteLine(day + "It is not a day");
+                    break;
+            }
+        }
+    }
+}
